e6243c0239baabd0f03dd2d18145f8f007510277
hieunumdum <hieunho500@gmail.com>
2026-04-11 13:16:45 +0700

    Delete project-App-Upload-Multi-File-UDM_11-dev directory

--- project-App-Upload-Multi-File-UDM_11-dev/Code/FileUploadServer/Models/FileMetadata.cs
+++
@@ -1,9 +0,0 @@
-namespace FileUploadServer.Models
-{
-    // ĐỊNH NGHĨA METADATA CỦA FILE (Để Server nhận biết)
-    public class FileMetadata
-    {
-        public string FileName { get; set; }
-        public long FileSize { get; set; }
-    }
-}
--- project-App-Upload-Multi-File-UDM_11-dev/PPTX/README.md
+++
@@ -1,9 +0,0 @@
-# Upload Multi File App
-
-This GUI application allows users to drag and drop multiple files to upload them to a previously connected server
-
----
-
-## Contributor
-
-- Name
